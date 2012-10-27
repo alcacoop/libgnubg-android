bf762efe425dfbc61559bf197be468419a4f1170
Domenico Martella <domenico.martella@alcacoop.it>
2012-10-27 21:23:45 +0200

    CLEANUP: init_rng refactoring and unused fns remotion

--- imported_functions.c
+++ imported_functions.c
@@ -59,20 +59,13 @@ extern void EvaluateRoll ( float ar[ NUM_ROLLOUT_OUTPUTS ], int nDie1, int nDie2
 
 extern void init_rng(void)
 {
-  if (!(rngctxCurrent = InitRNG(NULL, NULL, TRUE, rngCurrent))) {
-    printf("%s\n", _("Failure setting up RNG"));
-    exit(EXIT_FAILURE);
+  if (!rngctxCurrent) {
+    rngctxCurrent = InitRNG(NULL, NULL, TRUE, rngCurrent);
+    rngctxRollout = InitRNG(&rcRollout.nSeed, NULL, TRUE, rcRollout.rngRollout);
+    rcRollout.nSeed ^= 0x792A584B;
   }
-  if (!(rngctxRollout = InitRNG(&rcRollout.nSeed, NULL,
-                                TRUE, rcRollout.rngRollout))) {
-    printf("%s\n", _("Failure setting up RNG for rollout."));
-    exit(EXIT_FAILURE);
-  }
-  /* we don't want rollouts to use the same seed as normal dice (which
-     could happen if InitRNG had to use the current time as a seed) -- mix
-     it up a little bit */
-  rcRollout.nSeed ^= 0x792A584B;
 }
+
 
 extern void GetMatchStateCubeInfo( cubeinfo* pci, const matchstate* pms )
 {
@@ -81,51 +74,3 @@ extern void GetMatchStateCubeInfo( cubeinfo* pci, const matchstate* pms )
                pms->fJacoby, 3, pms->bgv );
 }
 
-extern void current_pmr_cubedata_update(evalsetup *pes, float output[][NUM_ROLLOUT_OUTPUTS], float stddev[][NUM_ROLLOUT_OUTPUTS])
-{
-	moverecord *pmr = get_current_moverecord(NULL);
-	if (!pmr)
-		return;
-	if (pmr->CubeDecPtr->esDouble.et == EVAL_NONE)
-		pmr_cubedata_set(pmr, pes, output, stddev);
-}
-
-extern moverecord *get_current_moverecord(int *pfHistory)
-{
-	if (plLastMove && plLastMove->plNext && plLastMove->plNext->p) {
-		if (pfHistory)
-			*pfHistory = TRUE;
-		return plLastMove->plNext->p;
-	}
-
-	if (pfHistory)
-		*pfHistory = FALSE;
-
-	if (ms.gs != GAME_PLAYING) {
-		pmr_hint_destroy();
-		return NULL;
-	}
-
-	/* invalidate on changed dice */
-	if (ms.anDice[0] > 0 && pmr_hint && pmr_hint->anDice[0] > 0 
-	    && (pmr_hint->anDice[0] != ms.anDice[0]
-		|| pmr_hint->anDice[1] != ms.anDice[1]))
-		pmr_hint_destroy();
-
-	if (!pmr_hint) {
-		pmr_hint = NewMoveRecord();
-		pmr_hint->fPlayer = ms.fTurn;
-	}
-
-	if (ms.anDice[0] > 0) {
-		pmr_hint->mt = MOVE_NORMAL;
-		pmr_hint->anDice[0] = ms.anDice[0];
-		pmr_hint->anDice[1] = ms.anDice[1];
-	} else if (ms.fDoubled) {
-		pmr_hint->mt = MOVE_TAKE;
-	} else {
-		pmr_hint->mt = MOVE_DOUBLE;
-		pmr_hint->fPlayer = ms.fTurn;
-	}
-	return pmr_hint;
-}
