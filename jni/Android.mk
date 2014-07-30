70e11242f5ff8d8c405b534acd0a0a0508f723c3
Domenico Martella <domenico.martella@alcacoop.it>
2014-07-30 23:33:16 +0200

    FIX: ndk build assets

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,1 +1,12 @@
-../Android.mk
+# This file is the top android makefile for all sub-modules.
+
+LOCAL_PATH := $(call my-dir)
+
+GLIB_TOP := $(LOCAL_PATH)/..
+LIBGNUBG_TOP := $(LOCAL_PATH)/../libgnubg
+
+include $(CLEAR_VARS)
+
+include $(GLIB_TOP)/glib/Android.mk
+include $(GLIB_TOP)/gthread/Android.mk
+include $(GLIB_TOP)/libgnubg/Android.mk
